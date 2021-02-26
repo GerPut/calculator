42017885152056ac93bae80c82e9faf53d6877b9
Gerhard <gerhard.putter@yahoo.com>
2021-02-26 12:23:33 +0200

    Calculator

---
+++ script.js
@@ -0,0 +1,28 @@
+//Variables
+const button1 = document.querySelector('.btn1').value
+const button2 = document.querySelector('.btn2').value
+
+//EventListeners
+
+
+//Functions
+
+function add(a, b) {
+    return a + b;
+}
+function subtract(a, b) {
+    return a - b;
+}
+function multiply(a, b) {
+    return a * b;
+}
+function devide(a, b) {
+    return a / b;
+}
+
+
+
+
+
+
+
